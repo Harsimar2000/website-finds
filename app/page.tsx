0931553cfd0ba95d6a0a84c878e66c5dc1bbf8d7
Harsimar <harsimar.rattan@gmail.com>
2024-04-09 23:02:54 -0300

    button changed color when hover over

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           </div>
           <div className="pt-5">
             <button
-              className="bg-black text-white font-bold py-2 px-4 rounded"
+              className="bg-black text-white font-bold py-2 px-4 rounded  hover:bg-gray-800"
               onClick={handleButtonClick}
             >
               Join Waitlist
